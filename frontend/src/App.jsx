dd3e1f7a126a9889654583f2d0241f6510374fb3
SinghSaheb44 <149952457+SinghSaheb44@users.noreply.github.com>
2025-03-25 01:37:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { Button } from './components/button';
-import EnrollForm from './Pages/EnrollForm'
 import StudentDashboard from "./Pages/StudentDashboard";
 import Home from "./Pages/HomePage";
 import AdminDashboard from "./Pages/admindashboard";
 import EducatorDashboard from "./Pages/EducatorDashboard";
 import LoginPage from "./Pages/LoginPage";
-import StudentList from  "./Pages/StudentList"
-import TeacherList from  "./Pages/TeacherList"
-import ProgramList from  "./Pages/ProgramList"
+import StudentList from "./Pages/StudentList"
+import TeacherList from "./Pages/TeacherList"
+import ProgramList from "./Pages/ProgramList"
+import Approvalrequest from "./Pages/Approvalrequest"
+import EnrollForm from './Pages/EnrollForm'
 //import { Link } from "react-router-dom";
 
 import AddStudentForm from "./Pages/AddStudentForm";
@@ -22,15 +23,15 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-     <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/enroll" element={<EnrollForm />} />
-         <Route path="/admin/student-list" element={<StudentList />} />
-         <Route path="/admin/educator-list" element={<TeacherList />} />
-         <Route path="/admin/program-list" element={<ProgramList />} />    
-       <Route path="/educator-dashboard" element={<EducatorDashboard />} />
+        <Route path="/admin/student-list" element={<StudentList />} />
+        <Route path="/admin/educator-list" element={<TeacherList />} />
+        <Route path="/admin/program-list" element={<ProgramList />} />
+        <Route path="/educator-dashboard" element={<EducatorDashboard />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
-       
+        <Route path="/admin/approval-requests" element={<Approvalrequest />} />
       </Routes>
     </Router>
   );
